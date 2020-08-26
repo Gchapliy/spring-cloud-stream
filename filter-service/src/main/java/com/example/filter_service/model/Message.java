6297c238d968f87ea89c9e1a07c0df6843803261
Gchapliy <grishachapliy1@gmail.com>
2020-08-26 14:35:56 +0300

    done

---
+++ filter-service/src/main/java/com/example/filter_service/model/Message.java
@@ -0,0 +1,12 @@
+package com.example.filter_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String message;
+}
